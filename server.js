005e92c3b64bbb3c53a540e1e7710c5636d3f58f
McCaul Baggett <mccaul0@gmail.com>
2012-02-24 12:16:07 -0500

    update with dashboard and css file

---
+++ server.js
@@ -0,0 +1,30 @@
+var http = require ('http'),
+	say;
+
+http.createServer(function (request, response) {
+    'use strict';
+		
+    if (request.headers.host === 'foo.localhost') {
+			
+			say = 'You asked for \"foo.localhost\"';
+			
+    } else if (request.headers.host === 'bar.localhost') {
+			
+			say = 'You asked for \"bar.localhost\"';
+			
+    } else {
+			
+			say = 'WTF?!';
+			
+    }
+		
+    response.writeHead(200, {
+			'Content-Type': 'text/plain'
+		});
+		
+		response.write(say);
+		response.end(say);
+    
+}).listen(80);
+
+console.log('Server is running on port 80');
